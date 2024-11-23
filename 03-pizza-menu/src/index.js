1479b751ca2a4fdea6cf6d890d7b6842e42707b7
TomatoesBurner <max683879@hotmail.com>
2024-11-23 10:23:32 +1000

    Setting Classes

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -106,11 +106,16 @@ function Menu() {
 
 function Pizza({ pizzaObj }) {
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
+        {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )} */}
         <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
